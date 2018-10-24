dadb28ef48fe24a783a4b054c0d2d06e07b78023
Jo Vansant <jo_vansant@hotmail.com>
2018-10-24 09:18:13 +0200

    commit2

---
+++ _test/_test/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _test
+{
+    class Class1
+    {
+        public string Text { get; set; }
+    }
+}
---
+++ _test/_test/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 msg = new Class1() { Text = "Hello World!" };
+            Console.WriteLine("hello world");
+            Console.WriteLine(msg.Text);
+
+            Console.ReadKey();
+        }
+    }
+}
